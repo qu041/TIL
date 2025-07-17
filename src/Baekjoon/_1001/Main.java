74a138522377b3a97cf6526f95ef8cce9c17858e
Quru <aprilfoolgy.0609@gmail.com>
2025-07-17 23:55:34 +0900

    2025-07-17 TIL: 백준 문제 풀이(입출력 & 사칙연산)

---
+++ src/Baekjoon/_1001/Main.java
@@ -0,0 +1,19 @@
+package Baekjoon._1001;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		System.out.println(a - b);
+	
+	}
+
+}
---
+++ src/Baekjoon/_2557/Main.java
@@ -0,0 +1,9 @@
+package Baekjoon._2557;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
