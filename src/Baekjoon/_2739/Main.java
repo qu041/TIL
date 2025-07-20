133becac08cc2bb234be431db23e7c58e909f0e3
Quru <aprilfoolgy.0609@gmail.com>
2025-07-20 21:10:44 +0900

    2025-07-20 TIL: 백준 문제 풀이(반복문)

---
+++ src/Baekjoon/_2739/Main.java
@@ -0,0 +1,21 @@
+package Baekjoon._2739;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		int N;
+		N = sc.nextInt();
+		
+		for (int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + (N*i));
+		}
+		
+		
+		
+		
+	}
+}
