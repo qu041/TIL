1fb28e28d41ffb650a321c6afd0a339a95f732f4
Quru <aprilfoolgy.0609@gmail.com>
2025-08-04 21:19:08 +0900

    2025-08-04 TIL: baekjoon 문제풀이(별찍기)

---
+++ Java/src/backjoon/p2439/Main.java
@@ -0,0 +1,27 @@
+package p2439;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for (int i=1; i<= n; i++) {
+			for (int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		 }
+		
+		scan.close();
+		
+		}
+
+}
