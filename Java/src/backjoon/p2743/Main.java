1ed212c7913d429c6f8346ecec47000ed54ab946
Quru <aprilfoolgy.0609@gmail.com>
2025-08-22 23:04:39 +0900

    2025-08-22 TIL : baekjoon 문제풀이(문자열)

---
+++ Java/src/backjoon/p2743/Main.java
@@ -0,0 +1,12 @@
+package p2743;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();      // 단어 입력
+        System.out.println(S.length());  // 단어 길이 출력
+    }
+}
