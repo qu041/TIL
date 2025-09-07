5fb81d4575dcec84d1257001d0110409a12df5f1
Quru <aprilfoolgy.0609@gmail.com>
2025-09-07 19:51:48 +0900

    2025-09-07 TIL: baekjoon 문제풀이(2차원배열)

---
+++ Java/src/backjoon/p2738/Main.java
@@ -0,0 +1,37 @@
+package p2738;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        // 행렬 A 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        // 행렬 B 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        // 결과 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
