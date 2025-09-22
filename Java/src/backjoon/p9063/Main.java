23db1265951977b2f63405d6bc043b2c8f47bb9e
Quru <aprilfoolgy.0609@gmail.com>
2025-09-22 21:34:20 +0900

    2025-09-22 TIL: baekjoon 문제풀이(기하)

---
+++ Java/src/backjoon/p9063/Main.java
@@ -0,0 +1,30 @@
+package p9063;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+
+        int area = (maxX - minX) * (maxY - minY);
+        System.out.println(area);
+    }
+}
+
