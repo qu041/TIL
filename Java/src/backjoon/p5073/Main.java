7224937dfadd05ff29d82cae8d90c3a2466ef6f3
Quru <aprilfoolgy.0609@gmail.com>
2025-09-24 22:18:38 +0900

    2025-09-24 TIL: baekjoon 문제풀이(기하) asd asd. git commit -m

---
+++ Java/src/backjoon/p5073/Main.java
@@ -0,0 +1,31 @@
+package p5073;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            
+        	int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == 0 && b == 0 && c == 0) break;
+
+            int max = Math.max(a, Math.max(b, c));
+
+            if (a + b + c - max <= max) {
+                System.out.println("Invalid");
+            } else if (a == b && b == c) {
+                System.out.println("Equilateral");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        }
+    }
+}
+
